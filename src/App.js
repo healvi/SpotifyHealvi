5f198c1edeee273f279cb809aa01b77accc493ed
healvi <57001337+healvi@users.noreply.github.com>
2022-03-19 18:01:27 +0700

    firstcommit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(process.env.REACT_APP_SPOTIFY_CLIENT_ID);
+
   return (
     <div className="App">
       <header className="App-header">
