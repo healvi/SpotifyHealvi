90f40aa8a20670614df63a9b765da7bb520c02cb
healvi <57001337+healvi@users.noreply.github.com>
2022-03-23 07:33:44 +0700

    session-1 v2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <div className="card-body">
           <h5 className="card-title">{data.album.name}</h5>
           <h6 className="card-subtitle mb-2 text-muted">{data.name}</h6>
-          <button onClick={select} type='button' className="btn btn-primary">Go somewhere</button>
+          <button onClick={select} type='button' className="btn btn-primary mt-3">Go somewhere</button>
         </div>
     </div>
     </div>
