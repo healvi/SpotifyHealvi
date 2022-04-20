eb951e1eb2d4526a846fdd4a9d94f63ea99dadbe
healvi <57001337+healvi@users.noreply.github.com>
2022-04-20 09:10:38 +0700

    add domain vercel app

--- src/utils/spotifyconf.js
+++ src/utils/spotifyconf.js
@@ -3,7 +3,7 @@ const RESPONSE_TYPE = 'token';
 const REDIRECT_URI = 'https://kelasgigih.vercel.app';
 const SCOPES = 'playlist-modify-public playlist-read-private playlist-modify-private';
 // eslint-disable-next-line no-undef
-const urlGet = `${AUTH_ENDPOINT}?client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_SPOTIFY_redirect_uri}&scope=${SCOPES}&response_type=${RESPONSE_TYPE}`;
+const urlGet = `${AUTH_ENDPOINT}?client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPES}&response_type=${RESPONSE_TYPE}`;
 
 export {
   AUTH_ENDPOINT, RESPONSE_TYPE, REDIRECT_URI, urlGet,
