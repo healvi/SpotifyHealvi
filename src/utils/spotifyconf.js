4b98b7498519e158174456c487328e90e4c83e23
healvi <57001337+healvi@users.noreply.github.com>
2022-04-20 08:58:22 +0700

    check vercel link

--- src/utils/spotifyconf.js
+++ src/utils/spotifyconf.js
@@ -1,8 +1,9 @@
 const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
 const RESPONSE_TYPE = 'token';
-const REDIRECT_URI = 'http://localhost:3000';
+const REDIRECT_URI = 'https://kelasgigih.vercel.app';
 const SCOPES = 'playlist-modify-public playlist-read-private playlist-modify-private';
-const urlGet = `${AUTH_ENDPOINT}?client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPES}&response_type=${RESPONSE_TYPE}`;
+// eslint-disable-next-line no-undef
+const urlGet = `${AUTH_ENDPOINT}?client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_SPOTIFY_redirect_uri}&scope=${SCOPES}&response_type=${RESPONSE_TYPE}`;
 
 export {
   AUTH_ENDPOINT, RESPONSE_TYPE, REDIRECT_URI, urlGet,
